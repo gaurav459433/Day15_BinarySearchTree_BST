cc9277bcda16ec53164fae89165236303bb51de4
gaurav459433 <gaurav459433@gmail.com>
2023-03-20 19:07:05 +0530

    Welcome To Data Structure Program Using Java Generics: Binary Search Tree-BST

---
+++ BinaryTreeMain.java
@@ -0,0 +1,8 @@
+package assignments259.dsbinarysearchtree;
+
+public class BinaryTreeMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Data Structure Program Using Java Generics: Binary Search Tree- BST");
+    }
+
+}
